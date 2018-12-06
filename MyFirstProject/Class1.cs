6a9c06951b29c7659f0d412c6cc2888f23d661fb
AlMahdavi <45404716+AlMahdavi@users.noreply.github.com>
2018-12-06 20:10:54 +0000

    importing project

---
+++ MyFirstProject/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+
+namespace MyFirstProject
+{
+    public class LoadBbc
+    {
+        static void main()
+        {
+            IWebDriver driver = new ChromeDriver();
+            Uri BBC = new Uri(@"Http://launcher.world0fwarcraft.com/alert");Uri U = new Uri(@"Http://launcher.world0fwarcraft.com/alert");
+        }
+    }
+
+}
